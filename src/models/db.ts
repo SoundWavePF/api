8ed863d7f759ea94a661c49e4172c08de8676fa2
Javier Ochoa <jochoaalv@gmail.com>
2022-06-07 16:01:00 -0500

    refactor(db): user-artist models

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,16 +1,3 @@
-// require('dotenv').config();
-// import { Sequelize } from 'sequelize-typescript'
-// import {Artist, User} from "./models";
-//
-// export const sequelize = new Sequelize({
-//     database: process.env.DB_NAME,
-//     dialect: 'postgres',
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     models: [Artist, User],
-//     logging: false
-// })
-
 'use strict';
 
 const fs = require('fs');
