757338999d367811491380d9a4fc8ff6c3b77032
Javier Ochoa <jochoaalv@gmail.com>
2022-06-07 15:58:39 -0500

    fix(db): user-artist models

--- src/models/Artist.ts
+++ src/models/Artist.ts
@@ -1,38 +1,97 @@
-import { Table, Column, Model, DataType, ForeignKey } from 'sequelize-typescript';
-import {User} from "./User";
+// import { Table, Column, Model, DataType, BelongsTo, ForeignKey } from 'sequelize-typescript';
+// import {User} from "./User";
+//
+// @Table({timestamps: true})
+// export class Artist extends Model{
+//
+//     @Column({
+//         type: DataType.INTEGER,
+//         allowNull: false,
+//         autoIncrement:true,
+//         primaryKey:true
+//     })
+//     id: number | undefined
+//
+//     @Column({
+//         type: DataType.STRING,
+//         allowNull: false,
+//         validate: {
+//             is: {
+//                 args: /^[A-Z a-z]+$/g,
+//                 msg: 'Name contains invalid characters'
+//             }
+//         }
+//     })
+//     name: string | undefined
+//
+//     @Column({type: DataType.INTEGER})
+//     plays: number | undefined
+//
+//     @Column({type: DataType.STRING, validate:{isUrl: true}})
+//     url_avatar: string | undefined
+//
+//     @ForeignKey(()=>User)
+//     @Column({
+//         type: DataType.INTEGER
+//     })
+//     userId: number | undefined
+//
+//     @BelongsTo(()=>User)
+//     @Column({
+//         type: DataType.INTEGER
+//     })
+//     user: User | undefined
+//
+//
+// }
 
-@Table({timestamps: false})
-export class Artist extends Model{
+'use strict'
 
-    @Column({
-        type: DataType.INTEGER,
-        allowNull: false,
-        autoIncrement:true,
-        primaryKey:true
-    })
-    id: number | undefined
+import {Model} from 'sequelize';
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-        validate: {
-            is: {
-                args: /^[A-Z a-z]+$/g,
-                msg: 'Name contains invalid characters'
+
+interface ArtistAttributes {
+    id: number;
+    name: string;
+    plays: number;
+    url_avatar: string;
+}
+
+module.exports = (sequelize:any, DataTypes:any)=>{
+    class Artist extends Model<ArtistAttributes>
+        implements ArtistAttributes{
+            id!: number;
+            name!: string;
+            plays!: number;
+            url_avatar!: string;
+            static associate(models: any){
+                Artist.belongsTo(models.User)
             }
+    }
+    Artist.init({
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is: {
+                    args: /^[A-Z a-z]+$/g,
+                    msg: 'Name contains invalid characters'
+                }
+            }
+        },
+        plays:{
+            type: DataTypes.INTEGER
+        },
+        url_avatar:{
+            type: DataTypes.STRING, validate:{isUrl: true}
         }
-    })
-    name: string | undefined
-
-    @Column({type: DataType.INTEGER})
-    plays: number | undefined
-
-    @Column({type: DataType.STRING, validate:{isUrl: true}})
-    url_avatar: string | undefined
-
-    @ForeignKey(()=>User)
-    @Column({
-        type: DataType.INTEGER
-    })
-    userId: number | undefined
+    }, {sequelize,
+        modelName: 'Artist'
+    });
+    return Artist;
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,60 +1,131 @@
-import { Table, Column, Model, DataType, ForeignKey} from 'sequelize-typescript';
-import {Artist} from "./Artist";
+// import { Table, Column, Model, DataType, ForeignKey} from 'sequelize-typescript';
+// import {Artist} from "./Artist";
+//
+// @Table({timestamps: false})
+// export class User extends Model {
+//
+//     @Column({
+//         type: DataType.INTEGER,
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true
+//     })
+//     id: number | undefined
+//
+//     @Column({
+//         type: DataType.STRING,
+//         allowNull: false,
+//         validate: {
+//             is: {
+//                 args: /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/igm,
+//                 msg: 'Username contains invalid characters'
+//             }
+//         }
+//     })
+//     username: string | undefined
+//
+//     @Column({
+//         type: DataType.STRING,
+//         allowNull: false,
+//         validate: {
+//             isEmail: {msg: "Invalid email"}
+//         }
+//     })
+//     email: string | undefined
+//
+//     @Column({
+//         type: DataType.STRING,
+//         allowNull: false,
+//         validate: {
+//             is: /^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{8,16}$/,
+//             msg: 'Invalid. It must not contain any whitespace, It must contain at least one uppercase, one lowercase and one numeric character. It must contain at least one special character. Length must be between 8 to 16 characters.'
+//         }
+//     })
+//     password: string | undefined
+//
+//     @Column({type: DataType.INTEGER})
+//     listeningTime: number | undefined
+//
+//     @Column({type: DataType.ENUM('visitant, registered, admin')})
+//     rol: string | undefined
+//
+//     @Column({type: DataType.STRING, validate: {isUrl: true}})
+//     url_avatar: string | undefined
+//
+//     @ForeignKey(() => Artist)
+//     @Column({
+//         type: DataType.INTEGER
+//     })
+//     artistId: number | undefined
+// }
 
-@Table({timestamps: false})
-export class User extends Model {
+'use strict'
 
-    @Column({
-        type: DataType.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-    })
-    id: number | undefined
+import {Model} from 'sequelize';
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-        validate: {
-            is: {
-                args: /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/igm,
-                msg: 'Username contains invalid characters'
+
+interface UserAttributes {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    rol: string;
+    url_avatar: string;
+}
+
+module.exports = (sequelize: any, DataTypes:any)=>{
+    class User extends Model<UserAttributes>
+        implements UserAttributes {
+            id!: number;
+            username!: string;
+            email!: string;
+            password!: string;
+            rol!: string;
+            url_avatar!: string;
+            static associate(models: any){
+                User.hasOne(models.Artist)
             }
+    }
+    User.init({
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is: {
+                    args: /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/igm,
+                    msg: 'Username contains invalid characters'
+                }
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: {msg: "Invalid email"}
+            }
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is: /^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{8,16}$/,
+                msg: 'Invalid. It must not contain any whitespace, It must contain at least one uppercase, one lowercase and one numeric character. It must contain at least one special character. Length must be between 8 to 16 characters.'
+            }
+        },
+        rol:{
+            type: DataTypes.ENUM('visitant, registered, admin')
+        },
+        url_avatar: {
+            type: DataTypes.STRING, validate: {isUrl: true}
         }
+    }, {sequelize,
+        modelName: 'User'
     })
-    username: string | undefined
-
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-        validate: {
-            isEmail: {msg: "Invalid email"}
-        }
-    })
-    email: string | undefined
-
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-        validate: {
-            is: /^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{8,16}$/,
-            msg: 'Invalid. It must not contain any whitespace, It must contain at least one uppercase, one lowercase and one numeric character. It must contain at least one special character. Length must be between 8 to 16 characters.'
-        }
-    })
-    password: string | undefined
-
-    @Column({type: DataType.INTEGER})
-    listeningTime: number | undefined
-
-    @Column({type: DataType.ENUM('visitant, registered, admin')})
-    rol: string | undefined
-
-    @Column({type: DataType.STRING, validate: {isUrl: true}})
-    url_avatar: string | undefined
-
-    @ForeignKey(() => Artist)
-    @Column({
-        type: DataType.INTEGER
-    })
-    artistId: number | undefined
+    return User;
 }
