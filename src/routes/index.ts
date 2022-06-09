dc2d9861923837cf73df64b6ee4c569dfe52ed76
Javier Ochoa <jochoaalv@gmail.com>
2022-06-09 12:12:42 -0500

    refactor: favorite search route + feature: add remove route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import {seederRouter} from "./seeder/seeder";
 import {searchRouter} from "./search/search";
+import {favoriteRouter} from "./user/favorite";
 
 export const routes = Router();
 
 routes.use('/seeder', seederRouter)
 routes.use('/search', searchRouter)
+routes.use('/favorite', favoriteRouter)
