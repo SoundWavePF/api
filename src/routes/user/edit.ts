afb956fbab26a5c69c06c33981acd2decaab82ce
Javier Ochoa <jochoaalv@gmail.com>
2022-06-14 20:47:37 -0500

    testWork

---
+++ src/routes/user/edit.ts
@@ -0,0 +1,11 @@
+// import { Router } from 'express';
+//
+// const editRouter = Router();
+//
+// editRouter.post('/update', async (req, res) => {
+//     const { id, field,  } = req.body;
+//     try {
+//         const user = await db.user.findOne({
+//             where: { id = id }
+//         });
+// }
