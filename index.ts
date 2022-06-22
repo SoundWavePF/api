d07cfd8c0734078ca1003c836e1905b8be46a5e2
cristian18u <98076070+cristian18u@users.noreply.github.com>
2022-06-21 21:36:59 -0500

    datos adicionales para favorites

--- index.ts
+++ index.ts
@@ -6,23 +6,17 @@ import axios from "axios";
 
 server.use(express.json())
 
-let reload = false;
+// let reload = false;
 
 // db.sequelize.sync({ force: true }).then(() => {
 db.sequelize.sync().then(() => {
     server.listen(process.env.PORT, async () => {
         console.log(`Listening in port ${process.env.PORT}`)
-    //     if(reload){
-    //         console.log('Creating DB...')
-    //         await axios.get(`http://localhost:3001/charge/one`)
-    //         await axios.get(`http://localhost:3001/charge/two`)
-    //     }
-    //     console.log('\n' +
-    //         '██████╗░██████╗░  ░█████╗░██████╗░███████╗░█████╗░████████╗███████╗██████╗░\n' +
-    //         '██╔══██╗██╔══██╗  ██╔══██╗██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗\n' +
-    //         '██║░░██║██████╦╝  ██║░░╚═╝██████╔╝█████╗░░███████║░░░██║░░░█████╗░░██║░░██║\n' +
-    //         '██║░░██║██╔══██╗  ██║░░██╗██╔══██╗██╔══╝░░██╔══██║░░░██║░░░██╔══╝░░██║░░██║\n' +
-    //         '██████╔╝██████╦╝  ╚█████╔╝██║░░██║███████╗██║░░██║░░░██║░░░███████╗██████╔╝\n' +
-    //         '╚═════╝░╚═════╝░  ░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═════╝░')
+        // if(reload){
+            // console.log('Creating DB...')
+            // await axios.get(`http://localhost:3001/charge/one`)
+            // await axios.get(`http://localhost:3001/charge/two`)
+        // }
+        // console.log('base de datos successfully')
     })
 });
