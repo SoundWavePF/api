0a12921277a6954b571a6e59ddbab4540fd60cdc
cristian18u <98076070+cristian18u@users.noreply.github.com>
2022-06-23 14:45:27 -0500

    res.send playlist modificado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,49 +1,72 @@
-import { Router } from 'express';
+import { Router } from "express";
 // import { seederRouter } from "./seeder/seeder";
 import send from "./send/send";
-import {searchRouter, artistRouter, albumRouter, genreRouter, topRouter, songRouter} from "./search";
-import {favoriteRouter, playlistRouter, registerRouter, loginRouter, updateRouter, artistRequestRouter, deactivateRouter, infoRouter, playRouter, historyRouter} from './user';
+import {
+	searchRouter,
+	artistRouter,
+	albumRouter,
+	genreRouter,
+	topRouter,
+	songRouter,
+} from "./search";
+import {
+	favoriteRouter,
+	playlistRouter,
+	registerRouter,
+	loginRouter,
+	updateRouter,
+	artistRequestRouter,
+	deactivateRouter,
+	infoRouter,
+	playRouter,
+	historyRouter,
+} from "./user";
 import link from "./link/link";
 import chargeJson from "./charge/chargeJson";
 import charge from "./charge/charge";
 import chargeTables from "./charge/chargeTables";
-import {adminRouter} from "./admin";
-import {artistSongRouter, artistAlbumRouter, artistProfileRouter, artistDescriptionRouter, artistStatsRouter} from "./artist";
-import {artistAsUserRouter} from "./seeder/loader";
-import {albumPatcher} from "./seeder/albumPatcher";
-
+import { adminRouter } from "./admin";
+import {
+	artistSongRouter,
+	artistAlbumRouter,
+	artistProfileRouter,
+	artistDescriptionRouter,
+	artistStatsRouter,
+} from "./artist";
+import { artistAsUserRouter } from "./seeder/loader";
+import { albumPatcher } from "./seeder/albumPatcher";
 
 export const routes = Router();
 
 // routes.use('/seeder', seederRouter)
-routes.use('/send', send)
-routes.use('/search', searchRouter)
-routes.use('/favorite', favoriteRouter)
-routes.use('/playlist', playlistRouter)
-routes.use('/link', link)
-routes.use('/chargeJson', chargeJson)
-routes.use('/charge', charge)
-routes.use('/chargedb', chargeTables)
+routes.use("/send", send);
+routes.use("/search", searchRouter);
+routes.use("/favorite", favoriteRouter);
+routes.use("/playlist", playlistRouter);
+routes.use("/link", link);
+routes.use("/chargeJson", chargeJson);
+routes.use("/charge", charge);
+routes.use("/chargedb", chargeTables);
 // routes.use('/chargeAlbum', chargeAlbum)
 // routes.use('/chargeGenre', chargeGenre)
-routes.use('/artist', artistRouter)
-routes.use('/album', albumRouter)
-routes.use('/genre', genreRouter)
-routes.use('/top', topRouter)
-routes.use('/register', registerRouter)
-routes.use('/login', loginRouter)
-routes.use('/update', updateRouter)
-routes.use('/admin', adminRouter)
-routes.use('/requestArtistStatus', artistRequestRouter)
-routes.use('/deactivate', deactivateRouter)
-routes.use('/info', infoRouter)
-routes.use('/artistpanel/album', artistAlbumRouter)
-routes.use('/artistpanel/song', artistSongRouter)
-routes.use('/artistpanel/profile', artistProfileRouter)
-routes.use('/play', playRouter)
-routes.use('/history', historyRouter)
-routes.use('/artistpanel/description', artistDescriptionRouter)
-routes.use('/asuser', artistAsUserRouter)
-routes.use('/song', songRouter)
-routes.use('/artistpanel/stats', artistStatsRouter)
-routes.use('/albumPatcher', albumPatcher)
+routes.use("/artist", artistRouter);
+routes.use("/album", albumRouter);
+routes.use("/genre", genreRouter);
+routes.use("/top", topRouter);
+routes.use("/register", registerRouter);
+routes.use("/login", loginRouter);
+routes.use("/update", updateRouter);
+routes.use("/admin", adminRouter);
+routes.use("/requestArtistStatus", artistRequestRouter);
+routes.use("/deactivate", deactivateRouter);
+routes.use("/info", infoRouter);
+routes.use("/artistpanel/album", artistAlbumRouter);
+routes.use("/artistpanel/song", artistSongRouter);
+routes.use("/artistpanel/profile", artistProfileRouter);
+routes.use("/play", playRouter);
+routes.use("/history", historyRouter);
+routes.use("/artistpanel/description", artistDescriptionRouter);
+routes.use("/asuser", artistAsUserRouter);
+routes.use("/song", songRouter);
+routes.use("/artistpanel/stats", artistStatsRouter);
+routes.use("/albumPatcher", albumPatcher);
--- src/routes/user/playlist.ts
+++ src/routes/user/playlist.ts
@@ -1,112 +1,148 @@
-import { Router } from 'express';
-import db from '../../models/db'
+import { Router } from "express";
+import db from "../../models/db";
 
 export const playlistRouter = Router();
 
-playlistRouter.post('/', async(req, res)=>{
-    try{
+playlistRouter.post("/", async (req, res) => {
+    try {
         const { email } = req.body;
-        console.log( req.body);
-        const user = await db.user.findOne({where: {email: email}});
-        
+        console.log(req.body);
+        const user = await db.user.findOne({ where: { email: email } });
+
         const userPlaylist = await db.playlist.findAll({
-            attributes :{exclude: ['userId']},
-            where: {userId: user.id}
-        })
-        return res.send(userPlaylist)
-    } catch (e:any) {
-        return res.send({message: e.message})
+            attributes: { exclude: ["userId"] },
+            where: { userId: user.id },
+        });
+        return res.send(userPlaylist);
+    } catch (e: any) {
+        return res.send({ message: e.message });
     }
+});
 
-})
-
-playlistRouter.get('/all', async(_req, res)=>{
-    try{
+playlistRouter.get("/all", async (_req, res) => {
+    try {
         const playlistOnDb = await db.playlist.findAll({
-            attributes :{exclude: ['userId']},
-            include: [{model: db.user, attributes: {exclude: ['email', 'password']}}]
-        })
-        return res.send(playlistOnDb)
-    } catch (e:any) {
-        return res.send({message: e.message})
+            attributes: { exclude: ["userId"] },
+            include: [
+                {
+                    model: db.user,
+                    attributes: { exclude: ["email", "password"] },
+                },
+            ],
+        });
+        return res.send(playlistOnDb);
+    } catch (e: any) {
+        return res.send({ message: e.message });
     }
-
-})
-playlistRouter.post('/create', async(req, res)=>{
+});
+playlistRouter.post("/create", async (req, res) => {
     const { email, playlistName } = req.body;
 
     try {
-        const user = await db.user.findOne({ where: { email: email } })
+        const user = await db.user.findOne({ where: { email: email } });
         const playlistCreated = await db.playlist.create({
-            "name": playlistName
-        })
-        user.addPlaylist(playlistCreated)
+            name: playlistName,
+        });
+        user.addPlaylist(playlistCreated);
 
-        return res.send({ message: `playlist: ${playlistName} created by user: ${email}` })
-    } catch (e:any) {
-        return res.send({message: e.message})
+        return res.send(playlistCreated);
+    } catch (e: any) {
+        return res.send({ message: e.message });
     }
-})
+});
 
-
-
-playlistRouter.post('/add', async (req, res) => {
+playlistRouter.post("/add", async (req, res) => {
     const { playlistId, songId } = req.body;
     try {
-        const playlist = await db.playlist.findOne({ where: { id: playlistId } })
-        const song = await db.song.findOne({ where: { id: songId } })
-        playlist.addSong(song)
+        const playlist = await db.playlist.findOne({
+            where: { id: playlistId },
+        });
+        const song = await db.song.findOne({ where: { id: songId } });
+        playlist.addSong(song);
         song.added_to_playlists += 1;
         song.save();
-        return res.send({ message: `song: ${song.name} has been added to playlist: ${playlist.name}` })
-    } catch (e:any) {
+        return res.send({
+            message: `song: ${song.name} has been added to playlist: ${playlist.name}`,
+        });
+    } catch (e: any) {
         // console.log(e)
-        return res.send({message: e.message})
+        return res.send({ message: e.message });
     }
-})
+});
 
-playlistRouter.post('/remove', async (req, res) => {
+playlistRouter.post("/remove", async (req, res) => {
     const { playlistId, songId } = req.body;
     try {
-        const playlist = await db.playlist.findOne({ where: { id: playlistId } })
-        const song = await db.song.findOne({ where: { id: songId } })
-        playlist.removeSong(song)
+        const playlist = await db.playlist.findOne({
+            where: { id: playlistId },
+        });
+        const song = await db.song.findOne({ where: { id: songId } });
+        playlist.removeSong(song);
         song.added_to_favorites -= 1;
         song.save();
-        return res.send({ message: `song: ${songId} has been removed from playlist: ${playlist.name}` })
+        return res.send({
+            message: `song: ${songId} has been removed from playlist: ${playlist.name}`,
+        });
     } catch (e) {
-        return res.send(e)
+        return res.send(e);
     }
-})
+});
 
-
-playlistRouter.post('/delete', async (req, res) => {
+playlistRouter.post("/delete", async (req, res) => {
     const { playlistId } = req.body;
     try {
-        const playlist = await db.playlist.findOne({ where: { id: playlistId }, include: [{model: db.song}] })
-        await Promise.all(playlist.songs.map(async (song:any)=>{
-            const songToDelete = await db.song.findOne({ where: { id: song.id } })
-            songToDelete.added_to_playlists -= 1;
-            songToDelete.save();
-        }));
+        const playlist = await db.playlist.findOne({
+            where: { id: playlistId },
+            include: [{ model: db.song }],
+        });
+        await Promise.all(
+            playlist.songs.map(async (song: any) => {
+                const songToDelete = await db.song.findOne({
+                    where: { id: song.id },
+                });
+                songToDelete.added_to_playlists -= 1;
+                songToDelete.save();
+            })
+        );
         playlist.destroy();
-        return res.send({ message: `playlist with id: ${playlistId} has been deleted` })
+        return res.send({
+            message: `playlist with id: ${playlistId} has been deleted`,
+        });
     } catch (e) {
-        return res.send(e)
+        return res.send(e);
     }
-})
+});
 
-playlistRouter.get('/:playlistId', async(req, res)=>{
+playlistRouter.get("/:playlistId", async (req, res) => {
     const { playlistId } = req.params;
-    try{
+    try {
         const playlistOnDb = await db.playlist.findOne({
-            attributes :{exclude: ['userId']},
-            where: {id: playlistId},
-            include: [{model: db.user, attributes: {exclude: ['email', 'password']}}, {model: db.song, attributes: {exclude: ['artist_id_reference', 'genre_id_reference', 'album_id_reference', 'albumId']}, include: [{model: db.album, attributes: ['name']}, {model:db.artist, attributes: ['name']}]}]
-        })
-        return res.send(playlistOnDb)
-    } catch (e:any) {
-        return res.send({message: e.message})
+            attributes: { exclude: ["userId"] },
+            where: { id: playlistId },
+            include: [
+                {
+                    model: db.user,
+                    attributes: { exclude: ["email", "password"] },
+                },
+                {
+                    model: db.song,
+                    attributes: {
+                        exclude: [
+                            "artist_id_reference",
+                            "genre_id_reference",
+                            "album_id_reference",
+                            "albumId",
+                        ],
+                    },
+                    include: [
+                        { model: db.album, attributes: ["name"] },
+                        { model: db.artist, attributes: ["name"] },
+                    ],
+                },
+            ],
+        });
+        return res.send(playlistOnDb);
+    } catch (e: any) {
+        return res.send({ message: e.message });
     }
-
-})
+});
