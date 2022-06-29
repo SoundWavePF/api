2148c0f1c99fbb572f37e6a9347fac8ab975c163
Javier Ochoa <jochoaalv@gmail.com>
2022-06-28 20:48:32 -0500

    refactor history response

--- src/routes/user/history.ts
+++ src/routes/user/history.ts
@@ -27,7 +27,7 @@ historyRouter.post('/', async (req, res) => {
             id: user.id,
             username: user.username,
             email: user.email,
-            history: history.reverse()
+            history: history.slice(0,20).reverse()
         }
         return res.send(obj);
     } catch (e:any) {
